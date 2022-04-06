67f0bec3b6bc7a55585dc87c021a0d92a501acc7
Luke Gaudiano <lukegaudiano@yahoo.com>
2022-04-06 13:44:03 -0500

    refresh

---
+++ src/static-date.js
@@ -0,0 +1,17 @@
+let items = [
+    {
+        id:0,
+        name:"Apple iPad",
+        descriptions:"overpriced piece of garbage",
+        price: 600.00
+    },
+    {
+        id:1,
+        name:"Apple iPad Pro",
+        descriptions:"super overpriced piece of garbage",
+        price: 800.00
+    }
+    
+];
+
+export {items};
